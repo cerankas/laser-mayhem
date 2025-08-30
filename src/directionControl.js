46590fcb3ab23885244d605bab8b81a1096f10a3
cerankas <cerankas@gmail.com>
2025-08-30 09:17:08 +0200

    fix touch control on mobile

--- src/directionControl.js
+++ src/directionControl.js
@@ -7,8 +7,11 @@ export class DirectionControl {
 
     this.angle = 0;
 
-    document.addEventListener('pointermove', e => {
-      this.angle = atan(this.x, this.y, e.offsetX, e.offsetY);
-    });
+    document.addEventListener('pointerdown', this.pointerEventHandler);
+    document.addEventListener('pointermove', this.pointerEventHandler);
+  }
+  
+  pointerEventHandler = e => {
+    this.angle = atan(this.x, this.y, e.offsetX, e.offsetY);
   }
 }
